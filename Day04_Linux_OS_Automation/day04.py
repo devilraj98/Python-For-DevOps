fb91d9fda9ee986b7c5ddf0ee340ed625143a73d
devilraj98 <neerajraj985275@gmail.com>
2026-01-05 23:05:05 +0530

    Day 4 updated

---
+++ Day04_Linux_OS_Automation/day04.py
@@ -0,0 +1,7 @@
+import os
+
+#OS automation matters in DevOps because it allows for efficient management and configuration of operating systems across multiple servers. This automation helps in reducing manual errors, speeding up deployment processes, and ensuring consistency in system configurations. By automating tasks such as software installation, updates, and system monitoring, DevOps teams can focus more on development and less on repetitive administrative tasks.
+
+#Get current working directory
+cwd = os.getcwd()
+print("Current Working Directory:", cwd)
